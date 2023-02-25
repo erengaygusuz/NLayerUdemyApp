d825dc7cbe5860dcad7e15337bdc5c8e0f48b6e7
Eren Gaygusuz <gaygusuzeren@gmail.com>
2023-02-25 10:46:07 +0300

    Seed Data Classes are Created

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -24,6 +24,24 @@ namespace NLayer.Repository
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
+            modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature()
+            {
+                Id = 1,
+                Color = "Kırmızı",
+                Height = 100,
+                Width = 200,
+                ProductId = 1
+            }, 
+            new ProductFeature()
+            {
+                Id = 2,
+                Color = "Mavi",
+                Height = 300,
+                Width = 200,
+                ProductId = 2
+            }
+            );
+
             base.OnModelCreating(modelBuilder);
         }
     }
