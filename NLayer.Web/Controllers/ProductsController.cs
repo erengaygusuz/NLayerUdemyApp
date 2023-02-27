95251e71a1929eafbeb46a128befae583ce015c6
Eren Gaygusuz <gaygusuzeren@gmail.com>
2023-02-27 19:25:02 +0300

    Product Delete Functionality is Added

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -86,5 +86,13 @@ namespace NLayer.Web.Controllers
 
             return View(productDto);
         }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            await _productService.RemoveAsync(product);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
