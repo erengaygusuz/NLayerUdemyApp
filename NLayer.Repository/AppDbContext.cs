657379b7af634bee03fc5969eb5fe8f09d141e7a
Eren Gaygusuz <gaygusuzeren@gmail.com>
2023-02-27 16:44:03 +0300

    Product Save Functionality is Added

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -1,18 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NLayer.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NLayer.Repository
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
-        { 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
 
         }
 
@@ -31,7 +26,7 @@ namespace NLayer.Repository
                 Height = 100,
                 Width = 200,
                 ProductId = 1
-            }, 
+            },
             new ProductFeature()
             {
                 Id = 2,
--- NLayer.Service/Services/Service.cs
+++ NLayer.Service/Services/Service.cs
@@ -3,12 +3,7 @@ using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
 using NLayer.Service.Exceptions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NLayer.Service.Services
 {
@@ -46,7 +41,7 @@ namespace NLayer.Service.Services
 
         public async Task<IEnumerable<T>> GetAllAsync()
         {
-            return await _repository.GetAll().ToListAsync(); 
+            return await _repository.GetAll().ToListAsync();
         }
 
         public async Task<T> GetByIdAsync(int id)
